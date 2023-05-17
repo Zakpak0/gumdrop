cf76a4a2c589be5bc921d90ca2c5687003fb6c38
Zakhary Oliver <me@zakharyoliver.com>
2023-05-17 05:50:04 -1000

    feat(gen): defined generate command

---
+++ cli/src/commands/generate/generate.ts
@@ -0,0 +1,72 @@
+import chalk from "chalk";
+import fs from "fs";
+import path from "path";
+import {
+  CommandInterface,
+  OpenAICommandInterface,
+} from "../../lib/command_interface";
+import { readConfig } from "../../lib/config";
+export async function generate(options: Record<string, any>): Promise<{
+  completed: boolean;
+  error?: string | undefined;
+}> {
+  const config = await readConfig();
+  let command_interface: CommandInterface;
+  const cwd = process.cwd();
+  if (config?.commandPath) {
+    const abs = path.resolve(cwd, config.commandPath ?? "");
+    const ci = require(abs).default;
+    command_interface = new ci();
+  } else {
+    command_interface = new OpenAICommandInterface();
+  }
+  const file = options?.file;
+  if (file == null || file.length == 0) {
+    console.warn(
+      `${chalk.red(
+        "Failed to doucment"
+      )} --file argument is empty \n ${chalk.red("recieved:")} ${file}`
+    );
+    process.exit(1);
+  } else {
+    const fileName = path.basename(file);
+    const generation = await command_interface.generateDocumentation(
+      readFile(file)
+    );
+    if (generation.completed) {
+      console.log(
+        `${chalk.green("Doucmented!")} ${fileName} \n ${chalk.green(
+          "recieved:"
+        )}`,
+        generation
+      );
+      return generation;
+    } else {
+      console.log(
+        `${chalk.red("Failed to doucment")} ${fileName} \n ${chalk.red(
+          "recieved:"
+        )}`,
+        generation
+      );
+      return generation;
+    }
+  }
+}
+
+export function readFile(file: string): string {
+  try {
+    const cwd = process.cwd();
+    const abs = path.resolve(cwd, file);
+    const contents = fs.readFileSync(abs, "utf-8");
+    return contents;
+  } catch (e) {
+    console.error(
+      `\n ${e} \n ${chalk.red(
+        "Failed to doucment"
+      )} --file argument was non-existant file path \n ${chalk.red(
+        "recieved:"
+      )} ${file}`
+    );
+    process.exit(1);
+  }
+}
---
+++ cli/src/commands/generate/index.ts
@@ -0,0 +1,16 @@
+import type { Command } from "commander";
+
+import { applyAnyAsyncAction } from "../../lib/async_command";
+
+export default function (program: Command) {
+  applyAnyAsyncAction(
+    program
+      .command("generate")
+      .description(
+        "Send documentation to a DocumentationServer for generation to DocumentationClient"
+      )
+      .alias("gen")
+      .option("-f, --file [string]", "Path to file to generate for"),
+    () => import("./generateAsync")
+  );
+}
